2f4221076233d7ce26059fc4b13967d364ee9f77
Elena Catalina Vrabie <catalina.vrabie@wavin.com>
2020-03-23 20:12:47 +0200

    First commit hehe:)

---
+++ TemaGit/src/firstCommit/TestGit.java
@@ -0,0 +1,9 @@
+package firstCommit;
+
+public class TestGit {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Git ! Denumirea proiectului de licență este  *Soluție informatică pentru încurajarea adoptării unui stil de viață sănătos* ");
+	}
+
+}
