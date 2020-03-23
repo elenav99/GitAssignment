ddc9500bddf1edba2aaad23e83a3387ab9fcdc3b
Elena Catalina Vrabie <catalina.vrabie@wavin.com>
2020-03-23 20:46:07 +0200

    Aplicatia foloseste date precum varsta, greutate si inaltime pentru a calcula necesarul zilnic de calorii. Am adaugat clasa Utilizator cu atributele varstaUtilizator, greutateInKilogrameUtilizator, inaltimeUtilizator si am generat un constructor care initializeaza toate campurile acestea.

---
+++ TemaGit/src/Licenta/Utilizator.java
@@ -0,0 +1,14 @@
+package Licenta;
+
+public class Utilizator {
+	int varstaUtilizator;
+	int greutateInKilogrameUtilizator;
+	int inaltimeUtilizator;
+	public Utilizator(int varstaUtilizator, int greutateInKilogrameUtilizator, int inaltimeUtilizator) {
+		super();
+		this.varstaUtilizator = varstaUtilizator;
+		this.greutateInKilogrameUtilizator = greutateInKilogrameUtilizator;
+		this.inaltimeUtilizator = inaltimeUtilizator;
+	}
+	
+}
